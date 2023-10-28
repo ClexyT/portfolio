fe92844ced41e0197f44bb2635228a1e325138be
ClexyT <eduardoandersen14@gmail.com>
2023-10-28 07:12:00 -0600

    navbar

---
+++ src/Components/NavApp.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import {Navbar,NavbarBrand,NavbarContent,NavbarItem,NavbarMenuToggle,NavbarMenu,NavbarMenuItem} from "@nextui-org/navbar";
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
+import english from "@/Translate/en/Global.json"
+import espanol from "@/Translate/es/Global.json"
+export default function NavApp() {
+  const en = english[0].en.navbar
+  const es = espanol[0].es.navbar
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isTranslate, setTranslate] = React.useState(false)
+  const enMenuItems = [
+    en.Home,
+    en.Portfolio,
+    en.About,
+    en.Contact
+  ];
+  const esMenuItems = [
+    es.Home,
+    es.Portfolio,
+    es.About,
+    es.Contact
+  ];
+  
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <h1 className=" cursor-default hover:text-blue-500">{isTranslate ? "My Portfolio" : "Mi Portafolio"}</h1>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#"></Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {[isTranslate ? esMenuItems : enMenuItems].map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === [isTranslate ? esMenuItems : enMenuItems].length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
